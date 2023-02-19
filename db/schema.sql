6c859712bb5ad6ca8ffe0ab54fa956edb541ae14
eavalo14 <avaloserick97@gmail.com>
2023-02-19 15:15:50 -0800

    adding schema for entire db

--- db/schema.sql
+++ db/schema.sql
@@ -8,12 +8,27 @@ CREATE TABLE departments (
   name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(19,2),
   department_id INT,
   FOREIGN KEY (department_id)
   REFERENCES departments(id)
+  ON DELETE SET NULL
+);
+
+
+CREATE TABLE employees (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id)
+  REFERENCES roles(id)
+  ON DELETE SET NULL,
+  FOREIGN KEY (manager_id)
+  REFERENCES employees(id)
   ON DELETE SET NULL
 );
